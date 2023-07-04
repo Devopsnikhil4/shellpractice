d54e98819d019e10072a8853da44933184fc17b9
Devopsnikhil4 <138021468+Devopsnikhil4@users.noreply.github.com>
2023-07-04 17:27:33 +0530

    Auto push

--- roboshop/components/frontend.sh
+++ roboshop/components/frontend.sh
@@ -5,3 +5,6 @@ if [ $ID -ne 0 ] ; then
    echo -e "\e[31m This script is expected to be run by a root user or with a sudo privilage \e[0m"
    exit 1
 fi
+
+echo "Installing Nginx :"
+yum install nginx -y
