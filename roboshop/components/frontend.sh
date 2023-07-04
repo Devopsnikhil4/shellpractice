2ebe053f19bbc4e73292eea215d97cec962e15a6
Devopsnikhil4 <138021468+Devopsnikhil4@users.noreply.github.com>
2023-07-04 18:15:31 +0530

    Auto push

--- roboshop/components/frontend.sh
+++ roboshop/components/frontend.sh
@@ -3,33 +3,31 @@
 COMPONENT=frontend
 ID=$(id -u)
 
+
 if [ $ID -ne 0 ] ; then
    echo -e "\e[31m This script is expected to be run by a root user or with a sudo privilage \e[0m"
    exit 1
 fi
 
+status(){
+    if [ $? -eq 0 ] ; then
+        echo -e "\e[32m success \e[0m"
+    else
+        echo -e "\e[31m failure \e[0m"
+        exit 2
+    fi
+}
+
 echo -n "Installing Nginx :"
 yum install nginx -y &>> "/tmp/${COMPONENT}.log"
-
-if [ $? -eq 0 ] ; then
-    echo -e "\e[32m success \e[0m"
-else
-    echo -e "\e[31m failure \e[0m"
-fi
+status $?
 
 echo -n "Dowloading the frontend zip file :"
 curl -s -L -o /tmp/frontend.zip "https://github.com/stans-robot-project/frontend/archive/main.zip"
-if [ $? -eq 0 ] ; then
-    echo -e "\e[32m success \e[0m"
-else
-    echo -e "\e[31m failure \e[0m"
-fi
+status $?
 
-echo -n "cleaning the file deleating the exisisting content in file"
-cd /usr/share/nginx/html
+echo -n "cleaning the file deleting the exisisting content in file"
+cd /usr/share/nginx/html "/tmp/${COMPONENT}.log"
 rm -rf *
-if [ $? -eq 0 ] ; then
-    echo -e "\e[32m success \e[0m"
-else
-    echo -e "\e[31m failure \e[0m"
-fi
+status $?
+
