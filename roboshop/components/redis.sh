a2fad4cd66fcffc828fcd98d0024a5280ff4dab7
Devopsnikhil4 <138021468+Devopsnikhil4@users.noreply.github.com>
2023-07-05 16:07:58 +0530

    Auto push

--- roboshop/components/redis.sh
+++ roboshop/components/redis.sh
@@ -6,9 +6,10 @@ source components/common.sh
 echo -e "****** \e[34m $COMPONENT Instatllation is Started \e[0m******"
 
 echo -n "Configuring $COMPONENT repo :"
-curl -L https://raw.githubusercontent.com/stans-robot-project/redis/main/redis.repo -o /etc/yum.repos.d/redis.repo &>> INandOUT
+curl -L https://raw.githubusercontent.com/stans-robot-project/$COMPONENT/main/$COMPONENT.repo -o /etc/yum.repos.d/$COMPONENT.repo &>> INandOUT
 status $?
 
-echo -n "Installing redis :"
-yum install redis-6.2.11 -y  &>> INandOUT
+echo -n "Installing $COMPONENT :"
+yum install $COMPONENT-6.2.11 -y  &>> INandOUT
 status $?
+
