fb9ca4c954475af5b0ec4a35f826317eb23fa141
Devopsnikhil4 <138021468+Devopsnikhil4@users.noreply.github.com>
2023-07-05 16:47:47 +0530

    Auto push

--- roboshop/components/user.sh
+++ roboshop/components/user.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
 COMPONENT=user
+APPUSER="roboshop"
+
 source components/common.sh
 
 echo -e "****** \e[34m $COMPONENT Instatllation is Started \e[0m******"
@@ -12,3 +14,43 @@ status $?
 echo -n "Installing NodeJs :"
 yum install nodejs -y &>> INandOUT
 status $?
+
+id $APPUSER &>> INandOUT
+if [ $? -ne 0 ] ; then
+    echo -n "Adding the service account :"
+    useradd $APPUSER &>> INandOUT
+    status $?
+fi
+
+echo -n "Downloading the $COMPONENT zip file :"
+curl -s -L -o /tmp/user.zip "https://github.com/stans-robot-project/user/archive/main.zip" &>> INandOUT
+status $?
+
+echo -n "copying the $COMPONENT to $APPUSER home directory :"
+cd /home/$APPUSER
+rm -rf $COMPONENT &>> INandOUT
+unzip /tmp/user.zip &>> INandOUT
+status $?
+
+echo -n "Moving the $COMPONENT-main to $COMPONENT :"
+mv $COMPONENT-main $COMPONENT
+status $?
+
+echo -n "npm install in $COMPONENT :"
+cd /home/$APPUSER/$COMPONENT
+npm install  &>> INandOUT
+status $?
+
+echo -n "Updatating the systemD file REDIS and  MONGODB Endpoints :"
+sed -i -e 's/REDIS_ENDPOINT/mongodb.roboshop.internal/' /home/$APPUSER/$COMPONENT/systemd.service
+sed -i -e 's/MONGO_DNSNAME/mongodb.roboshop.internal/' /home/$APPUSER/$COMPONENT/systemd.service
+mv /home/$APPUSER/$COMPONENT/systemd.service /etc/systemd/system/$COMPONENT.service
+status $?
+
+echo -n "Starting the ${COMPONENT} service :"
+systemctl daemon-reload       &>> INandOUT
+systemctl enable $COMPONENT   &>> INandOUT
+systemctl restart $COMPONENT  &>> INandOUT
+status $?
+
+echo -e "****** \e[34m $COMPONENT Instatllation is Completed \e[0m******"
