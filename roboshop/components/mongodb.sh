2d61f4cd35daf5229f424e4083ba484fef8f30a3
Devopsnikhil4 <138021468+Devopsnikhil4@users.noreply.github.com>
2023-07-05 12:00:30 +0530

    Auto push

--- roboshop/components/mongodb.sh
+++ roboshop/components/mongodb.sh
@@ -20,14 +20,20 @@ status(){
 }
 
 echo -n "Configuring $COMPONENT repo :"
-curl -s -o /etc/yum.repos.d/mongodb.repo https://raw.githubusercontent.com/stans-robot-project/mongodb/main/mongo.repo  &>> INandOUT
+curl -s -o /etc/yum.repos.d/mongodb.repo https://raw.githubusercontent.com/stans-robot-project/mongodb/main/mongo.repo
 status $?
 
 echo -n "Installing $COMPONENT :"
-yum install -y $COMPONENT-org  &>> INandOUT
+yum install -y $COMPONENT-org    &>> INandOUT
+status $?
+
+echo -n "Updatating the Listen IP in the config file :"
+sed -i -e 's/127.0.0.1/0.0.0.0/'/etc/mongod.conf
 status $?
 
 echo -n "starting the $COMPONENT :"
-systemctl enable mongod  &>> INandOUT
-systemctl start mongod   &>> INandOUT
+systemctl daemon-reload mongod   &>> INandOUT
+systemctl enable mongod          &>> INandOUT
+systemctl restart mongod         &>> INandOUT
 status $?
+
