2a26fa4ee3f3b6ac43a73a281956f0c86d4aaaff
Devopsnikhil4 <138021468+Devopsnikhil4@users.noreply.github.com>
2023-07-05 15:38:21 +0530

    Auto push

--- roboshop/components/mongodb.sh
+++ roboshop/components/mongodb.sh
@@ -1,22 +1,8 @@
 #!/bin/bash
 
 COMPONENT=mongodb
-ID=$(id -u)
-INandOUT="/tmp/${COMPONENT}.log"
 
-if [ $ID -ne 0 ] ; then
-   echo -e "\e[31m This script is expected to be run by a root user or with a sudo privilage \e[0m"
-   exit 1
-fi
-
-status(){
-    if [ $? -eq 0 ] ; then
-        echo -e "\e[32m success \e[0m"
-    else
-        echo -e "\e[31m failure \e[0m"
-        exit 2
-    fi
-}
+source components/common.sh
 
 echo -e "****** \e[34m $COMPONENT Instatllation is Started \e[0m******"
 
