ac0dfe084da393bea741a4268dcb4f1539bb1684
Devopsnikhil4 <138021468+Devopsnikhil4@users.noreply.github.com>
2023-07-05 12:01:57 +0530

    Auto push

--- roboshop/components/mongodb.sh
+++ roboshop/components/mongodb.sh
@@ -28,7 +28,7 @@ yum install -y $COMPONENT-org    &>> INandOUT
 status $?
 
 echo -n "Updatating the Listen IP in the config file :"
-sed -i -e 's/127.0.0.1/0.0.0.0/'/etc/mongod.conf
+sed -i -e 's/127.0.0.1/0.0.0.0/' /etc/mongod.conf
 status $?
 
 echo -n "starting the $COMPONENT :"
