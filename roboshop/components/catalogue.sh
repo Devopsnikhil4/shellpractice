ae9ca0b332fb5f63c17d54a4be7937f47da15ba5
Devopsnikhil4 <138021468+Devopsnikhil4@users.noreply.github.com>
2023-07-05 12:52:18 +0530

    Auto push

--- roboshop/components/catalogue.sh
+++ roboshop/components/catalogue.sh
@@ -19,3 +19,11 @@ status(){
 }
 
 echo -e "****** \e[34m $COMPONENT Instatllation is Started \e[0m******"
+
+echo -n "Configuring $COMPONENT repo :"
+curl --silent --location https://rpm.nodesource.com/setup_16.x | sudo bash - &>> INandOUT
+status $?
+
+echo -n "Installing the nodejs :"
+yum install nodejs -y &>> INandOUT
+status $?
