7e3d18143004be435c1320296874b0e1db67706b
Devopsnikhil4 <138021468+Devopsnikhil4@users.noreply.github.com>
2023-07-05 11:34:48 +0530

    Auto push

--- roboshop/components/mongodb.sh
+++ roboshop/components/mongodb.sh
@@ -1,3 +1,25 @@
 #!/bin/bash
 
+COMPONENT=mongodb
+ID=$(id -u)
+INandOUT="/tmp/${COMPONENT}.log"
+
+
+if [ $ID -ne 0 ] ; then
+   echo -e "\e[31m This script is expected to be run by a root user or with a sudo privilage \e[0m"
+   exit 1
+fi
+
+status(){
+    if [ $? -eq 0 ] ; then
+        echo -e "\e[32m success \e[0m"
+    else
+        echo -e "\e[31m failure \e[0m"
+        exit 2
+    fi
+}
+
+echo -n "Configuring $COMPONENT repo :"
+curl -s -o /etc/yum.repos.d/mongodb.repo https://raw.githubusercontent.com/stans-robot-project/mongodb/main/mongo.repo  &>> INandOUT
+status $?
 
