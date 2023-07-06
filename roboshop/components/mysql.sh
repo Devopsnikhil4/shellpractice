cb6b6417847baa0e4515f5e23b8d16fadcc80415
Devopsnikhil4 <138021468+Devopsnikhil4@users.noreply.github.com>
2023-07-06 16:03:47 +0530

    Auto Push

--- roboshop/components/mysql.sh
+++ roboshop/components/mysql.sh
@@ -24,8 +24,8 @@ DEFAULT_ROOT_PASSWORD=$(grep 'temporary password'  /var/log/mysqld.log | awk '{p
 status $?
 
 echo "show databases;" | mysql -uroot -pRoboShop@1 &>> INandOUT
-if [ $? -ne 0 ] ; then
-    echo -n "Perfoming password reset of root user :"
+echo -n "Perfoming password reset of root user :"
+ if [ $? -ne 0 ] ; then
     echo "ALTER USER 'root'@'localhost' IDENTIFIED BY 'RoboShop@1';" | mysql --connect-expired-password -uroot -p$DEFAULT_ROOT_PASSWORD &>> INandOUT
     status $?
 fi
