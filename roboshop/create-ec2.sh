bb13d7e28a0df728016c577138805cae952d8671
Devopsnikhil4 <138021468+Devopsnikhil4@users.noreply.github.com>
2023-07-08 22:18:31 +0530

    Auto Push

--- roboshop/create-ec2.sh
+++ roboshop/create-ec2.sh
@@ -3,6 +3,7 @@
 #AMI_ID="ami-0c1d144c8fdd8d690"
 
 COMPONENT=$1
+ENV=$2
 HOSTEDZONEID="Z06982071MSU6YCTWUDBT"
 
 if [ -z "$1" ] ; then
@@ -22,13 +23,13 @@ echo -e "AMI ID is used to launch the EC2 is \e[32m $AMI_ID \e[0m"
 echo -e "SG ID is used to launch the EC2 is \e[32m $SG_ID \e[0m"
 echo -e "\e[34m *****Launch the server*****\e[0m"
 
-IPADDRESS=$(aws ec2 run-instances  --image-id $AMI_ID --instance-type t3.micro --security-group-ids $SG_ID --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$COMPONENT}]" | jq '.Instances[].PrivateIpAddress' |sed -e 's/"//g')
+IPADDRESS=$(aws ec2 run-instances  --image-id $AMI_ID --instance-type t3.micro --security-group-ids $SG_ID --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$COMPONENT-$ENV}]" | jq '.Instances[].PrivateIpAddress' |sed -e 's/"//g')
 
-echo -e "\e[34m ***** Launching $COMPONENT server is Completed *****\e[0m"
-echo -e "Private Address of $COMPONENT is \e[35m $IPADDRESS \e[0m"
+echo -e "\e[34m ***** Launching $COMPONENT-$ENV server is Completed *****\e[0m"
+echo -e "Private Address of $COMPONENT-$ENV is \e[35m $IPADDRESS \e[0m"
 
 echo -e "\e[34m ***** Creating DNS Record for the $COMPONENT : ***** \e[0m"
-sed -e "s/COMPONENT/${COMPONENT}/" -e "s/IPADDRESS/${IPADDRESS}/" route53.json > /tmp/record.json
+sed -e "s/COMPONENT/${COMPONENT}-${ENV}/" -e "s/IPADDRESS/${IPADDRESS}/" route53.json > /tmp/record.json
 aws route53 change-resource-record-sets --hosted-zone-id $HOSTEDZONEID --change-batch file:///tmp/record.json
 echo -e "\e[34m ***** Creating DNS Record for the $COMPONENT has completed : ***** \e[0m \n\n"
 
