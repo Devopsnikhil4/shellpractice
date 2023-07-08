5f3146c85529808117df2b524fd7055e3322cfbd
Devopsnikhil4 <138021468+Devopsnikhil4@users.noreply.github.com>
2023-07-08 13:00:47 +0530

    Auto Push

--- roboshop/create-ec2.sh
+++ roboshop/create-ec2.sh
@@ -12,7 +12,7 @@ echo -e "SG ID is used to launch the EC2 is \e[32m $SG_ID \e[0m"
 
 echo -e "\e[34m *****Launch the server*****\e[0m"
 
-aws ec2 run-instances  --image-id $AMI_ID --instance-type t3.micro --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$COMPONENT}]" | jq .
+aws ec2 run-instances  --image-id $AMI_ID --instance-type t3.micro --security-group-ids $SG_ID --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$COMPONENT}]" | jq .
     
 
 
