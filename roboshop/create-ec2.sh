1dd6da2a1813ca29f50a2b3cb09b2903cdf1f4a9
Devopsnikhil4 <138021468+Devopsnikhil4@users.noreply.github.com>
2023-07-08 15:01:46 +0530

    Auto Push

--- roboshop/create-ec2.sh
+++ roboshop/create-ec2.sh
@@ -25,8 +25,9 @@ echo -e "Private Address of $COMPONENT is \e[35m $IPADDRESS \e[0m"
 echo -e "\e[34m ***** Launching $COMPONENT server is Completed *****\e[0m"
 
 echo -n "\e[34m ***** Creating DNS Record for the $COMPONENT: ***** \e[0m"
+
 sed -e "s/COMPONENT/${COMPONENT}/" -e "s/IPADDRESS/${IPADDRESS}/" route53.json > /tmp/record.json
-aws route53 change-resource-record-sets --hosted-zone-id $HOSTEDZONEID --change-batch file://tmp/record.json
+aws route53 change-resource-record-sets --hosted-zone-id $HOSTEDZONEID --change-batch file:///tmp/record.json
 
 
 
